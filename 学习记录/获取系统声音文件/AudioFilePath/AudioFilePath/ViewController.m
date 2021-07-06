c024cfbf576149eb8a4861f8b968a9ad6e96cc80
tangbailin <634522497@qq.com>
2021-07-06 14:19:02 +0800

    获取系统声音路径，并调用系统播放方法

---
+++ 学习记录/获取系统声音文件/AudioFilePath/AudioFilePath/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  AudioFilePath
+//
+//  Created by tangbailin on 2021/7/5.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
